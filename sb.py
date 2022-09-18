923c928c98018a9c1b3a0feaa915d7463abb0246
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2022-09-18 11:06:00 -0300

    Add copyright and disclaimer

--- sb.py
+++ sb.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python
+
+# (c) Fábio Rodrigues Ribeiro - http://farribeiro.blogspot.com
+
+# Copying and distribution of this file, with or without modification, are
+# permitted in any medium without royalty provided the copyright notice and this
+# notice are preserved.  This file is offered as-is, without any warranty.
+
 import os
 import sys
 import subprocess as sp
