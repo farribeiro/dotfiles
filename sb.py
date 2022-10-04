247d24a9084c50bc50458f7e37494bce2c417736
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2022-10-04 18:20:43 -0300

    Just a hint

--- sb.py
+++ sb.py
@@ -24,7 +24,7 @@ class sbrebase:
 				# case "reinstall":
 					# os.system("rpm-ostree upgrade --install=akmod-nvidia-470xx --install=xorg-x11-drv-nvidia-470xx --install=xorg-x11-drv-nvidia-470xx-power --install=nvidia-settings-470xx --install=rtl88x2bu-kmod --install=flatpak-builder")
 				case "change":
-					os.system("rpm-ostree rebase fedora:fedora/",self.__sbversion+1,"/x86_64/silverblue --uninstall=rpmfusion-free-release-",self.__sbversion,"-1.noarch --uninstall=rpmfusion-nonfree-release-",self.__sbversion,"-1.noarch --install=https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-",self.__sbversion+1,".noarch.rpm --install=https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-",self.__sbversion+1,".noarch.rpm")
+					print("Syntax:\n\nrpm-ostree rebase fedora:fedora/",self.__sbversion+1,"/x86_64/silverblue --uninstall=rpmfusion-free-release-",self.__sbversion,"-1.noarch --uninstall=rpmfusion-nonfree-release-",self.__sbversion,"-1.noarch --install=https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-",self.__sbversion+1,".noarch.rpm --install=https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-",self.__sbversion+1,".noarch.rpm")
 				case "cleanall":
 					os.system("rpm-ostree cleanup -p -b -m")
 				case "preview":
