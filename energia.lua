ecfff63ede004005fcf2ef4d6c1e0154024f816b
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2022-10-06 10:31:25 -0300

    Change to tabs

--- energia.lua
+++ energia.lua
@@ -1,36 +1,36 @@
 co = coroutine.create (function ()
 
 function potencia(amp,volts)
-    return amp * volts
+	return amp * volts
 end
 
 function potencia4kwh(ptnc)
-    return ptnc / 1000
+	return ptnc / 1000
 end
 
 function custo4hora()
-    return KWh * preco
+	return KWh * preco
 end
 
 function custo4dia()
-    return custo4hora() * horas4dia
+	return custo4hora() * horas4dia
 end
 
 function custo4mes()
-    return custo4dia() * dias
+	return custo4dia() * dias
 end
 
 function kwh4mes()
-    return KWh * horas4dia * dias
+	return KWh * horas4dia * dias
 end
 
 function estimativas()
-    io.write("\n**********ESTIMATIVAS GLOBAIS**********\n")
-    io.write(KWh, "KWh x ", preco, "= R$ ", custo4hora(), "/hora\n")
-    io.write("Custo de R$ ", custo4dia(), "/dia\n")
-    io.write("Custo de R$ ", custo4mes(), "/mês (24x7)\n")
-    io.write(KWh, " x ", horas4dia, " x ", dias, " = ", kwh4mes(), "kWh/mês\n")
-    io.write("***************************************\n\n")
+	io.write("\n**********ESTIMATIVAS GLOBAIS**********\n")
+	io.write(KWh, "KWh x ", preco, "= R$ ", custo4hora(), "/hora\n")
+	io.write("Custo de R$ ", custo4dia(), "/dia\n")
+	io.write("Custo de R$ ", custo4mes(), "/mês (24x7)\n")
+	io.write(KWh, " x ", horas4dia, " x ", dias, " = ", kwh4mes(), "kWh/mês\n")
+	io.write("***************************************\n\n")
 end
 
 horas4dia = 24
@@ -39,9 +39,9 @@ dias = 30
 io.write("Preço(KWh): ")
 preco = io.read("*number")
 -- try:
---     amp = float(input("Amperes: "))
---     volts = float(input("Voltagem: "))
---     ptnc = self.potencia(amp,volts)
+	-- amp = float(input("Amperes: "))
+	-- volts = float(input("Voltagem: "))
+	-- ptnc = self.potencia(amp,volts)
 -- except ValueError:
 io.write("Potência(W): ")
 ptnc = io.read("*number")
