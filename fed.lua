494d3be0ed0b965c06cb86bdb6ebefddd96cbf37
Fábio Rodrigues Ribeiro <farribeiro@gmail.com>
2023-12-07 21:37:38 -0300

    Create fed.lua

---
+++ fed.lua
@@ -0,0 +1,35 @@
+#!/usr/bin/lua
+
+-- SPDX-License-Identifier: GPL-2.0
+-- Copyright 2022-2023 - Fábio Rodrigues Ribeiro and contributors
+
+handlers = {
+	["off"] = function()
+		os.execute("semanage boolean -m --off selinuxuser_execheap")
+	end,
+
+	["in"] = function()
+		os.execute("rpm-ostree upgrade --install=make --install=libtirpc --install=libtirpc-devel --install=policycoreutils-python-utils --install=gcc --install=python3-fedora --install=neofetch && systemctl reboot")
+	end,
+
+	["kr"] = function()
+		os.execute([[cd ~/kernel-tests;
+		sudo -s <<< "semanage boolean -m --on selinuxuser_execheap &&\
+		git pull &&\
+		./runtests.sh &&\
+		./runtests.sh -t performance &&\
+		semanage boolean -m --off selinuxuser_execheap" &&\
+		echo "" &&\
+		neofetch distro de cpu gpu memory]])
+	end
+}
+
+-- Extra functions
+handlers["off-selinux"] = handlers["off"]
+
+if not arg or #arg == 0 then
+	handlers["help"]()
+	os.exit(1)
+else
+	handlers[arg[1]]()
+end
