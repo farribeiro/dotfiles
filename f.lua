2cf577a15d18d471f69b4a3d57784f051a2c1405
Fábio Rodrigues Ribeiro <farribeiro@gmail.com>
2024-01-13 19:13:13 -0300

    sudo isnt needed and break line

--- f.lua
+++ f.lua
@@ -24,7 +24,8 @@ handlers = {
 		os.execute(([[mkdir -p ~/work/kernel_test &&\
 		cd ~/work/kernel_test &&\
 		koji download-build --arch=$(uname -m) kernel-%s-300.fc39 &&\ 
-		sudo rpm-ostree override replace kernel-modules-core-6*.rpm\
+		rpm-ostree override replace\
+		kernel-modules-core-6*.rpm\
 		kernel-core-6*.rpm\
 		kernel-modules-6*.rpm\
 		kernel-6*.rpm\
