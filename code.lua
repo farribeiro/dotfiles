7b7059f5ef4b12e280181e3f315ce67ecc603dc0
Fábio Rodrigues Ribeiro <farribeiro@gmail.com>
2024-01-26 15:08:27 -0300

    Add flatpak vscode command line script

---
+++ code.lua
@@ -0,0 +1,6 @@
+#!/usr/bin/lua
+
+-- SPDX-License-Identifier: GPL-2.0
+-- Copyright 2022-2023 - Fábio Rodrigues Ribeiro and contributors
+
+os.execute(("flatpak run com.vscodium.codium %s"):format(not arg or #arg == 0 and " " or arg[1]))
