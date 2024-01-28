441862bebb25d2b4f19fd7c312a1e7194827809a
Fábio Rodrigues Ribeiro <farribeiro@gmail.com>
2024-01-28 00:00:48 -0300

    add pwd env

--- code.lua
+++ code.lua
@@ -3,4 +3,4 @@
 -- SPDX-License-Identifier: GPL-2.0
 -- Copyright 2022-2023 - Fábio Rodrigues Ribeiro and contributors
 
-os.execute(("flatpak run com.vscodium.codium %s"):format(not arg or #arg == 0 and " " or arg[1]))
+os.execute(("flatpak run com.vscodium.codium %s"):format(not arg or #arg == 0 and " " or ("%s/%s"):format(os.getenv("PWD"), arg[1])))
