4b86e29aa74af9a00fc1c3f57ca59e18f42d6b5d
Fábio Rodrigues Ribeiro <farribeiro@gmail.com>
2024-02-05 15:57:35 -0300

    add gitbulk script

---
+++ gitbulk.lua
@@ -0,0 +1,23 @@
+#!/usr/bin/lua
+
+-- SPDX-License-Identifier: GPL-2.0
+-- Copyright 2022-2024 - Fábio Rodrigues Ribeiro and contributors
+
+local handlers = {
+	["clone"] = function ()
+		local file = io.open("git.txt", "r")
+		if not file then error(("Erro ao abrir o arquivo: %s"):format(filename)) end
+		for line in file:lines() do os.execute(("git clone %s"):format(line)) end
+		file:close()
+	end,
+	
+	["pull"] = function()
+		os.execute("find . -maxdepth 1 -type d -exec bash -c \"cd '{}' && git pull\" \\;")
+	end
+}
+
+if not arg or #arg == 0 then
+	-- handlers["help"]()
+	os.exit(1)
+end
+handlers[arg[1]]()
