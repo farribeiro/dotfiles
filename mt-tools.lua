054fb7e1272b9d2a12b64bb087352f6a8c50c942
Fábio Rodrigues Ribeiro <farribeiro@gmail.com>
2024-02-07 14:36:24 -0300

    rework and add count lines

--- mt-tools.lua
+++ mt-tools.lua
@@ -5,32 +5,21 @@
 
 local function readLinesFromFile(filename)
 	local file = io.open(filename, "r")
-	if not file then
-			error("Erro ao abrir o arquivo: " .. filename)
-	end
-
+	if not file then error("Erro ao abrir o arquivo: " .. filename) end
 	local lines = {}
-	for line in file:lines() do
-			table.insert(lines, line)
-	end
+	for line in file:lines() do table.insert(lines, line) end
 
 	file:close()
 	return lines
 end
 
 local function cloneRepositories(filename)
-	local repositories = readLinesFromFile(filename)
-
-	for _, repo in ipairs(repositories) do
-			os.execute(("git clone %s"):format(repo))
-	end
+	for _, repo in ipairs(readLinesFromFile(filename)) do os.execute(("git clone %s"):format(repo)) end
 end
 
 local function writeWorldMtFile(modules)
 	local file = io.open("world.mt", "w")
-	if not file then
-			error("Erro ao abrir o arquivo world.mt para escrita.")
-	end
+	if not file then error("Erro ao abrir o arquivo world.mt para escrita.") end
 
 	file:write("gameid = minetest\nworld_name =\n\n")
 	file:write(table.concat(modules, "\n"))
@@ -44,14 +33,21 @@ local handlers ={
 		local repositoriesFile = "git.txt"
 		local modulesFile = "modules.txt"
 
+		local count_repo = 0
+		for _ in ipairs(readLinesFromFile(repositoriesFile)) do count_repo = count_repo + 1 end
+
 		cloneRepositories(repositoriesFile)
 
+		local count_modules = 0
 		local modules = readLinesFromFile(modulesFile)
 		for i, item in ipairs(modules) do
-				modules[i] = ("load_mod_%s = true"):format(modules[i])
+			modules[i] = ("load_mod_%s = true"):format(modules[i])
+			count_modules = count_modules +1
 		end
 
 		writeWorldMtFile(modules)
+
+		io.write(("Quantidade de repositórios: %d\nQuantidade de mods: %d\n"):format(count_repo, count_modules))
 	end,
 
 	["up-mods"] = function()
