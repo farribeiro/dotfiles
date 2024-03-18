a72fb6ccfa9d3e9f7507f4a4927b3371e31b324d
Fábio Rodrigues Ribeiro <farribeiro@gmail.com>
2024-03-17 22:25:22 -0300

    rework

--- obs.lua
+++ obs.lua
@@ -5,20 +5,35 @@
 
 local input_file = "entrada.md"
 local output_file = "rascunho.md"
+local patterns = {}
 
 -- Função para abrir um arquivo, realizar a substituição e gravar o conteúdo modificado em um novo arquivo
-local function open_replace(pattern, new_pattern)
-  local f = assert(io.open(input_file, "r")) -- Abre o arquivo de entrada para leitura
-  local md = f:read("*all") -- Lê todo o conteúdo do arquivo
-  f:close() -- Fecha o arquivo de entrada
+local function open_replace()
+	local f = assert(io.open(input_file, "r")) -- Abre o arquivo de entrada para leitura
+	local md = f:read("*all") -- Lê todo o conteúdo do arquivo
+	f:close() -- Fecha o arquivo de entrada
 
-  f = assert(io.open(output_file, "w")) -- Abre o arquivo de saída para escrita
-  f:write(md:gsub(pattern, new_pattern)) -- Escreve o conteúdo modificado no arquivo de saída -- Aplica a substituição
-  f:close() -- Fecha o arquivo de saída
+	os.execute(("rm -rf %s"):format(output_file))
+	f = assert(io.open(output_file, "w")) -- Abre o arquivo de saída para escrita
+	for i, item in ipairs(patterns) do
+		md = md:gsub(item[1], item[2]) -- Escreve o conteúdo modificado no arquivo de saída -- Aplica a substituição
+	end
+
+	f:write(md)
+	f:close() -- Fecha o arquivo de saída
+end
+
+local function add_patterns(p)
+	table.insert(patterns, p)
 end
 
 handlers ={
-  ["dots"] = function() open_replace("%.%.%.", "…") end -- Função para substituir os pontos suspensivos por um caractere especial. (... por … )
+	["all"] = function()
+		add_patterns({"%.%.%.", "…"})
+		add_patterns({" ", " "})
+		open_replace()
+	end,
+	["dots"] = function() add_patterns({"%.%.%.", "…"}) open_replace() end -- Função para substituir os pontos suspensivos por um caractere especial. (... por … )
 }
 
 
