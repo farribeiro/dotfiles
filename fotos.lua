206769e0d1676808ebfd74b5ba7bc11b7dc3c838
Fábio Rodrigues Ribeiro <farribeiro@gmail.com>
2024-03-17 23:36:10 -0300

    rework

--- fotos.lua
+++ fotos.lua
@@ -6,16 +6,16 @@
 local x = os.execute
 
 local function getoutput()
-		local handle = io.popen([[\ls -1]])
-		local result = handle:read("*a")
-		handle:close()
-		return result:gmatch("[^\n]+")
+	local handle = io.popen [[\ls -1]]
+	local result = handle:read "*a"
+	handle:close()
+	return result:gmatch "[^\n]+"
 end
 
 local function mkdir() x("mkdir -p resultado") end
 
 local function corre(file, opt)
-	if file:match("%.avif$") or file:match("%.jpg$") or file:match("%.jpeg$") or file:match("%.png$") or file:match("%.webp$") or file:match("%.jxl$") then x(("convert %s"):format(opt)) end
+	if file:match "%.avif$" or file:match "%.jpg$" or file:match "%.jpeg$" or file:match "%.png$" or file:match "%.webp$" or file:match "%.jxl$" then x(("convert %s"):format(opt)) end
 end
 
 local function resize(size)
@@ -27,8 +27,8 @@ local function normalize_ppi()
 end
 
 handlers ={
-	["resize-800600"] = function() resize("800x600") end,
-	["resize-hd"] = function() resize("1280x720") end,
+	["resize-800600"] = function() resize "800x600" end,
+	["resize-hd"] = function() resize "1280x720" end,
 	["normalize-ppi"] = function() normalize_ppi() end
 }
 
