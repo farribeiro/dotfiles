15b008825b166f53db9c1928f96c53f778b59b79
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2024-07-16 12:00:57 -0300

    trocado os.execute por x

--- gitbulk.lua
+++ gitbulk.lua
@@ -7,16 +7,12 @@ local x = os.execute
 
 local handlers = {
 	["clone"] = function ()
-		local file = io.open("git.txt", "r")
-		if not file then error(("Erro ao abrir o arquivo: %s"):format(filename)) end
-		for line in file:lines() do os.execute(("git clone %s"):format(line)) end
+		local file = assert(io.open("git.txt", "r"))
+		for line in file:lines() do x(("git clone %s"):format(line)) end
 		file:close()
 	end,
 	["sm"] = function() x "git submodule update && git submodule update --init" end,
-	
-	["pull"] = function()
-		os.execute("find . -maxdepth 1 -type d -exec bash -c \"cd '{}' && git pull\" \\;")
-	end
+	["pull"] = function() x "find . -maxdepth 1 -type d -exec bash -c \"cd '{}' && git pull\" \\;" end
 }
 
 if not arg or #arg == 0 then
