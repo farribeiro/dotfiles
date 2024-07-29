98ff6981eaf25abf19df3d08357a63ff606d3226
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2024-07-29 13:04:05 -0300

    Change to zed

--- code.lua
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/lua
-
--- SPDX-License-Identifier: GPL-2.0
--- Copyright 2022-2023 - Fábio Rodrigues Ribeiro and contributors
-
-os.execute(("flatpak run com.vscodium.codium %s"):format(not arg or #arg == 0 and " " or ("%s/%s"):format(os.getenv("PWD"), arg[1])))
