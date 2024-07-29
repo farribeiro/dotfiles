ca05dc4b27fa4bf25bb7593ec59474fff0e03ba7
Fábio Rodrigues Ribeiro <farribeiro@gmail.com>
2024-07-29 18:19:25 -0300

    use conditional attribuitiob

--- meme.lua
+++ meme.lua
@@ -3,5 +3,5 @@
 -- SPDX-License-Identifier: GPL-2.0
 -- Copyright 2023 - Fábio Rodrigues Ribeiro and contributors
 
-if not arg or #arg == 0 then os.exit(1) end
+a = not arg or #arg == 0 and os.exit(1) or
 os.execute(("ffmpeg -i %s -vf \"split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse\" -loop 0 %s"):format(arg[1], arg[1]:gsub("%.mp4", ".avif")))
