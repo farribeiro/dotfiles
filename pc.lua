1773a021a1eca48950877a073ffdfa6823fb9303
Fábio Rodrigues Ribeiro <farribeiro@gmail.com>
2024-09-09 18:21:28 -0300

    Simplifies

--- pc.lua
+++ pc.lua
@@ -11,7 +11,7 @@ local filename = "list.txt"
 local file = assert(io.open(filename, "w"))
 if not file then error "Erro ao abrir o arquivo world.mt para escrita." end
 
-for linha in string.gmatch(result, "[^\n]+") do file:write(("file '%s'\n"):format(linha)) end  -- Imprime diretamente a linha no formato 'file'
+for linha in result:gmatch("[^\n]+") do file:write(("file '%s'\n"):format(linha)) end  -- Imprime diretamente a linha no formato 'file'
 
 file:close()
 print (("%s criado com sucesso."):format(filename))
