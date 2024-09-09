d7ca993f043378802ecb062c2ce2895eefdfcdfe
Fábio Rodrigues Ribeiro <farribeiro@gmail.com>
2024-09-09 18:16:42 -0300

    Simplifies

--- pc.lua
+++ pc.lua
@@ -3,7 +3,7 @@
 -- SPDX-License-Identifier: GPL-2.0
 -- Copyright 2024 - Fábio Rodrigues Ribeiro and contributors
 
-local handle = io.popen("\\ls -1")
+local handle = io.popen "\\ls -1"
 local result = handle:read "*a"
 handle:close()
 
@@ -11,7 +11,6 @@ local filename = "list.txt"
 local file = assert(io.open(filename, "w"))
 if not file then error "Erro ao abrir o arquivo world.mt para escrita." end
 
-print (result)
 for linha in string.gmatch(result, "[^\n]+") do file:write(("file '%s'\n"):format(linha)) end  -- Imprime diretamente a linha no formato 'file'
 
 file:close()
