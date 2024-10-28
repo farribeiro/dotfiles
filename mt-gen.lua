52157a5240b22de37a4bab7284d5bd521f66a381
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2024-10-28 14:18:04 -0300

    Change the dir

--- mt-gen.lua
+++ mt-gen.lua
@@ -4,7 +4,7 @@
 -- Copyright 2023 - Fábio Rodrigues Ribeiro and contributors
 
 os.execute [[
-cd ~/net.minetest.Minetest ;\
+cd ~/fkmtcustom ;\
 git pull &&\
 git submodule update --init &&\
 flatpak run org.flatpak.Builder --install --user --force-clean build-dir net.minetest.Minetest.yaml
