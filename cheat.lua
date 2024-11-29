e03caaa2c2f4da5879f453c5e7d99952aa8079ea
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2024-11-29 11:51:09 -0300

    Adicione :list como padrão

--- cheat.lua
+++ cheat.lua
@@ -3,4 +3,4 @@
 -- SPDX-License-Identifier: GPL-2.0
 -- Copyright 2023 - Fábio Rodrigues Ribeiro and contributors
 
-os.execute(("curl \"cht.sh/lua/%s\""):format(not arg or #arg == 0 and "" or arg[1]))
+os.execute(("curl \"cht.sh/lua/%s\""):format(not arg or #arg == 0 and ":list" or arg[1]))
