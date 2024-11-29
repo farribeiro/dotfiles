d2261b67a700fd37c75b2d592249af96e6d51109
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2024-11-29 15:46:29 -0300

    Elimina variavel

--- scripts/caracoroa.lua
+++ scripts/caracoroa.lua
@@ -1,3 +1,2 @@
 math.randomseed(os.time())
-local n = math.random(1, 2)
-io.write (("Saiu %s\n"):format(n == 1 and "Cara" or "Coroa"))
+io.write (("Saiu %s\n"):format(math.random(1, 2) == 1 and "Cara" or "Coroa"))
