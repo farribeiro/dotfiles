81a836564b9d5a970db626b3acf5928fad5f8cf9
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2024-11-30 22:02:20 -0300

    adicionado #! e copyright

--- scripts/caracoroa.lua
+++ scripts/caracoroa.lua
@@ -1,2 +1,7 @@
+#!/usr/bin/lua
+
+-- SPDX-License-Identifier: GPL-2.0
+-- Copyright 2024 - Fábio Rodrigues Ribeiro and contributors
+
 math.randomseed(os.time())
 io.write (("Saiu %s\n"):format(math.random(1, 2) == 1 and "Cara" or "Coroa"))
