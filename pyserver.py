eae4e885781e41a382234fd0f88adb89074e9bac
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2024-11-30 22:04:43 -0300

    Adicionado script do http.server do python

---
+++ pyserver.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+# SPDX-License-Identifier: GPL-2.0
+# Copyright 2024 - Fábio Rodrigues Ribeiro and contributors
+
+import os
+
+class Pyserver:
+	def __init__(self): os.system("python -m http.server")
+
+p = Pyserver()
