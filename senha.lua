0a3c4af8633277feba9f2e28f286bed7aad67f27
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2024-12-02 21:35:17 -0300

    Trocado nome da variavel para ficar pequena

--- senha.lua
+++ senha.lua
@@ -4,8 +4,8 @@
 -- Copyright 2024 - Fábio Rodrigues Ribeiro and contributors
 
 local tamanho = 12 -- Você pode alterar o tamanho aqui
-local caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+=<>?/{}[]|"
+local chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+=<>?/{}[]|"
 local senha = ""
 math.randomseed(os.time()) -- Semente para números aleatórios
-for _ = 1, tamanho do local i = math.random(1, #caracteres) senha = senha .. caracteres:sub(i, i) end
+for _ = 1, tamanho do local i = math.random(1, #chars) senha = senha .. chars:sub(i, i) end
 print("Senha gerada: " .. senha) -- Exibir a senha gerada
