b122a47a1364ffc08b165507cb565d482cd7ef3d
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2024-12-20 05:56:51 -0300

    Movido para uma função local e adicionado a tabela

--- util.lua
+++ util.lua
@@ -1,19 +1,19 @@
 -- SPDX-License-Identifier: GPL-2.0
 -- Copyright 2022-2024 - Fábio Rodrigues Ribeiro and contributors
 
+local function getoutput(cmd)
+		local handle = io.popen(cmd)
+		if not handle then return nil, "Failed to execute command" end
+		local result = handle:read "*a"
+		handle:close()
+		if not result or result == "" then return nil, "Command output is empty" end
+		return result
+end
+
 return {
 ["sbversion"] = function() return getoutput "rpm -E %fedora" end,
 
 ["arch"] = function() return getoutput "uname -m":gsub("[\n\r]", "") end,
-
-["getoutput"] = function(cmd)
-	local handle = io.popen(cmd)
-	if not handle then return nil, "Failed to execute command" end
-	local result = handle:read("*a")
-	handle:close()
-	if not result or result == "" then return nil, "Command output is empty" end
-	return result
-end,
-
 ["write_x"]= function(cmd) io.write(cmd .. "\n") os.execute(cmd) end
+getoutput = getoutput
 }
