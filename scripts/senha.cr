b6622b3d06abafba5c7f36130893fa9678e62dd9
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2024-12-22 13:00:02 -0300

    Refeito o programa

--- scripts/senha.cr
+++ scripts/senha.cr
@@ -1,13 +1,27 @@
 # SPDX-License-Identifier: GPL-2.0
 # Copyright 2024 - Fábio Rodrigues Ribeiro and contributors
 
-tamanho = 12 # Tamanho da senha - Você pode alterar o tamanho aqui
-chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+=<>?/{}[]|" # Conjunto de caracteres
-senha = "" # Inicializando a senha
+class Password
+	def generate
+		print "Entre com o tamanho da senha: "
+		len = gets.to_s.to_i # Tamanho da senha - Você pode alterar o tamanho aqui
 
-# Gerando a senha
-senha = String.build(tamanho) do |io|
-	tamanho.times do io.write_byte chars.to_slice.sample random: Random::Secure end
+		chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+=<>?/{}[]|" # Conjunto de caracteres
+		pass = "" # Inicializando a senha
+
+		# Gerando a senha
+		pass = String.build(len) do |io|
+			len.times do io.write_byte chars.to_slice.sample random: Random::Secure end
+		end
+
+		puts "Senha gerada: #{pass}" # Exibir a senha gerada
+	end
+
+	def initialize
+		#unless ARGV.empty?
+			generate
+		#end
+	end
 end
 
-puts "Senha gerada: #{senha}" # Exibir a senha gerada
+p = Password.new
