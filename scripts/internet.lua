ce996119bfb8c359fe31387399bbf4a97e3521d9
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2024-12-22 13:40:14 -0300

    Reorganizado a disposição das mensagens

--- scripts/internet.lua
+++ scripts/internet.lua
@@ -14,16 +14,16 @@ local function tombits(size) return increase_bits(size) end
 local function tokbits(size) return tombits(size) * 10 end
 local function tombytes(size) return changebits2bytes(tombits(size)) end
 local function togbytes(size) return changebits2bytes(size) end
+local function writeinbits(valor, desc) io.write(("Tamanho da banda é %.f %s\n"):format(valor, desc)) end
+function writeKiB() writeinbits(tokbits(number), "Kilobits (KiB)") end
 
 line = lerTamanho("Entre com o tamanho da internet: ")
 number, option = line:gsub("[%a]", ""), line:gsub("[^%a]", "")
 
 if option == "G" then
-	io.write("Tamanho da banda é ", tombits(number), " Megabits\n")
-	io.write("Tamanho da banda é ", tokbits(number), " Kilobits\n")
-	io.write("Taxa de download é ", tombytes(number), " Megabytes")
-elseif option == "M" then
-	io.write("Tamanho da banda é ", tokbits(number), " Kilobits")
-end
+	writeinbits (tombits(number), " Megabits (MiB)")
+	writeKiB()
+elseif option == "M" then writeKiB() end
 
+io.write("Taxa de download é ", tombytes(number), " Megabytes (MB)")
 io.write("\n")
