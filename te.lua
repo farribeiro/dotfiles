854d1fecf64b8fc8884569a00636c4d5cfa541a9
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2025-01-05 13:01:39 -0300

    Acresentado argumentos

--- te.lua
+++ te.lua
@@ -3,4 +3,5 @@
 -- SPDX-License-Identifier: GPL-2.0
 -- Copyright 2024 - Fábio Rodrigues Ribeiro and contributors
 
-os.execute "toolbox enter"
+local s = require "sai"
+os.execute(("toolbox enter %s"):format(s.ca() and "" or arg[1]))
