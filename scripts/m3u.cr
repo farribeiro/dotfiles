b7daf02662a8fe161b95c381300a841be042d28a
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2025-01-05 13:14:41 -0300

    Retirado classe

--- scripts/m3u.cr
+++ scripts/m3u.cr
@@ -1,18 +1,13 @@
-class M3u
-	def initialize
-		unless ARGV.empty?
-			filename = String.build(2) do |io|
-				io << ARGV[0] << ".m3u"
-			end
 
-			File.open(filename, "w") do |file| # Abre o arquivo no modo "write" (escrita), criando-o se não existir
-				file.puts "#EXTM3U"
-				file.puts ARGV[1]
-				file.puts
-			end
-			puts "#{filename} gravado com sucesso!"
-		end
+unless ARGV.empty?
+	filename = String.build(2) do |io|
+		io << ARGV[0] << ".m3u"
 	end
-end
 
-m3u = M3u.new
+	File.open(filename, "w") do |file| # Abre o arquivo no modo "write" (escrita), criando-o se não existir
+		file.puts "#EXTM3U"
+		file.puts ARGV[1]
+		file.puts
+	end
+	puts "#{filename} gravado com sucesso!"
+end
