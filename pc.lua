62d8cc75142296fa1fc91aa532508ea40f26f7b3
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2025-01-11 17:22:56 -0300

    Use variavel x para os.execute e economize caracteres

--- pc.lua
+++ pc.lua
@@ -3,7 +3,8 @@
 -- SPDX-License-Identifier: GPL-2.0
 -- Copyright 2024 - Fábio Rodrigues Ribeiro and contributors
 
-os.execute("rm -rf list.txt output.flac output.mp3")
+local x = os.execute
+x "rm -rf list.txt output.flac output.mp3"
 local handle = assert(io.popen "\\ls -1 | sort -V")
 if not handle then error "Erro ao executar o comando." end
 local result = handle:read "*a"
@@ -18,4 +19,4 @@ for linha in result:gmatch("[^\n]+") do file:write(("file '%s'\n"):format(linha)
 file:close()
 print (("%s criado com sucesso."):format(filename))
 
-os.execute("ffmpeg -f concat -safe 0 -i list.txt -c:a flac output.flac")
+x "ffmpeg -f concat -safe 0 -i list.txt -c:a flac output.flac"
