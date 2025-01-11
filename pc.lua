aea50837c63ee9245145104dfa77fb7ef55c4495
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2025-01-11 17:18:45 -0300

    Coloca ordenação natural da saída ls

--- pc.lua
+++ pc.lua
@@ -3,7 +3,7 @@
 -- SPDX-License-Identifier: GPL-2.0
 -- Copyright 2024 - Fábio Rodrigues Ribeiro and contributors
 
-local handle = assert(io.popen "\\ls -1")
+local handle = assert(io.popen "\\ls -1 | sort -V")
 if not handle then error "Erro ao executar o comando." end
 local result = handle:read "*a"
 handle:close()
