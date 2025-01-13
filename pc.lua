295fffbbfeb0d70fc3b56658d5a6c1e4a6416b2c
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2025-01-13 20:34:21 -0300

    Colocado em função chamada arquivos

--- pc.lua
+++ pc.lua
@@ -4,10 +4,6 @@
 -- Copyright 2024 - Fábio Rodrigues Ribeiro and contributors
 
 x = os.execute
-local handle = assert(io.popen "\\ls -1 | sort -V")
-if not handle then error "Erro ao executar o comando." end
-local result = handle:read "*a"
-handle:close()
 
 local function limpa()
 	x "rm -rf list.txt output.flac output.mp3 *.mp3"
@@ -16,6 +12,13 @@ local filename = "list.txt"
 local file = assert(io.open(filename, "w"))
 if not file then error ("Erro ao abrir o arquivo %s para escrita."):format(filename) end
 
+local function arquivos()
+	local handle = assert(io.popen "\\ls -1 | sort -V")
+	if not handle then error "Erro ao executar o comando." end
+	local result = handle:read "*a"
+	handle:close()
+	return result
+end
 for linha in result:gmatch("[^\n]+") do file:write(("file '%s'\n"):format(linha)) end  -- Imprime diretamente a linha no formato 'file'
 
 file:close()
