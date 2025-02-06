bbec62411447e4d1ebc1ffc7689e18b2a5afb393
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2025-02-06 10:47:11 -0300

    Adiciona script para gerar iso de CD/DVD

---
+++ mkiso.lua
@@ -0,0 +1,8 @@
+#!/usr/bin/lua
+
+-- SPDX-License-Identifier: GPL-2.0
+-- Copyright 2025 - Fábio Rodrigues Ribeiro and contributors
+
+local sai = require "sai"
+sai.ca_none()
+os.execute(("sudo dd if=/dev/sr0 of=%s/%s.iso bs=4M status=progress"):format(os.getenv("PWD"), arg[1]))
