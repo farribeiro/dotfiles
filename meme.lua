994c9b03cf6f776cf39bd5b0f2fd2d3cef420876
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2025-02-06 10:42:25 -0300

    Remove a dependencia de modulo externo

--- meme.lua
+++ meme.lua
@@ -3,7 +3,6 @@
 -- SPDX-License-Identifier: GPL-2.0
 -- Copyright 2023 - Fábio Rodrigues Ribeiro and contributors
 
-local sai = require "sai"
+if not arg or #arg == 0 then io.write("Nenhum argumento foi passado, saindo...\n") os.exit(1) end
 
-sai.ca_none()
 os.execute(("ffmpeg -i %s -vf \"split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse\" -loop 0 %s"):format(arg[1], arg[1]:gsub("%.mp4", ".avif")))
