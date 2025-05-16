eebc059df25fbbf8e8c52e1698066dd14a566583
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2025-05-16 19:54:16 -0300

    Adicionado script synct

---
+++ synct.lua
@@ -0,0 +1,6 @@
+#!/usr/bin/lua
+
+-- SPDX-License-Identifier: GPL-2.0
+-- Copyright 2025 - Fábio Rodrigues Ribeiro and contributors
+
+os.execute "toolbox run syncthing & flatpak run com.microsoft.Edge http://127.0.0.1:8384/#"
