5b681add7f8c2932c432ac32fcba46dc69238ed0
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2025-07-29 11:48:42 -0300

    Add a fedora dependecies script

---
+++ fdep.lua
@@ -0,0 +1,18 @@
+#!/usr/bin/lua
+
+-- SPDX-License-Identifier: GPL-2.0
+-- Copyright 2023-2024 - Fábio Rodrigues Ribeiro and contributors
+
+local x = os.execute
+
+local handlers = {
+    ["findfile"] = function () x(("dnf provides %s"):format(arg[2])) end,
+	["whatsdepends"] = function () x(("dnf repoquery --whatdepends %s"):format(arg[2])) end
+}
+
+handlers["ff"] = handlers["findfile"]
+handlers["wd"] = handlers["whatsdepends"]
+
+
+if require"sai":ca() then handlers["help"]() os.exit(1) end
+handlers[arg[1]]()
