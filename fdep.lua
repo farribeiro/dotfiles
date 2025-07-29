73a271c07948babf54f35bd6c5e07b7276d7ffc4
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2025-07-29 13:30:15 -0300

    Add new function

--- fdep.lua
+++ fdep.lua
@@ -7,11 +7,13 @@ local cmd_rq = "dnf -q repoquery "
 
 local handlers = {
     ["filepackage"] = function (q) return ("dnf provides %s"):format(q) end,
+    ["showdeps"] = function (q) return cmd_rq .. ("%s --providers-of=depends"):format(q) end,
 	["whatdepends"] = function (q) return cmd_rq .. ("--whatdepends %s"):format(q) end
 }
 
 handlers["fp"] = handlers["filepackage"]
 handlers["wd"] = handlers["whatdepends"]
+handlers["sd"] = handlers["showdeps"]
 
 if require"sai":ca() then handlers["help"]() os.exit(1) end
 os.execute(handlers[arg[1]](arg[2]))
