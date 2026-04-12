07c67cef9808bd723db50fd26e5bd3300afad726
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2026-04-12 16:42:56 -0300

    Autoformater

--- obs.lua
+++ obs.lua
@@ -10,30 +10,35 @@ local patterns = {}
 -- Abrir um arquivo, realizar a substituição e gravar o conteúdo modificado em um novo arquivo
 local function open_replace()
 	local f = assert(io.open(input_file, "r")) -- Abre o arquivo de entrada para leitura
-	local md = f:read "*all" -- Lê todo o conteúdo do arquivo
-	f:close() -- Fecha o arquivo de entrada
+	local md = f:read "*all"                -- Lê todo o conteúdo do arquivo
+	f:close()                               -- Fecha o arquivo de entrada
 
 	os.execute(("rm -rf %s"):format(output_file))
-	f = assert(io.open(output_file, "w")) -- Abre o arquivo de saída para escrita
+	f = assert(io.open(output_file, "w"))                              -- Abre o arquivo de saída para escrita
 	for i, item in ipairs(patterns) do md = md:gsub(item[1], item[2]) end -- Escreve o conteúdo modificado no arquivo de saída -- Aplica a substituição
 
 	f:write(md)
 	f:close() -- Fecha o arquivo de saída
 end
 
-local function add_patterns(s, r) table.insert(patterns, {s, r}) end
+local function add_patterns(s, r) table.insert(patterns, { s, r }) end
 local function espaco() add_patterns(" ", " ") end -- Espaços alienigenas por espaços simples.
 local function dash() add_patterns("%-%-", "–") end -- Dois menos por dash. ( -- por – )
 local function trave() add_patterns("%-%-%-", "—") end -- Três menos por travessão. ( --- por — )
 local function dots() add_patterns("%.%.%.", "…") end -- Pontos suspensivos por um caractere especial. (... por … )
 
-local handlers ={
+local handlers = {
 	["espaco"] = espaco,
 	["dash"] = dash,
 	["trave"] = trave,
 	["dots"] = dots,
 	-- ["run"] = function() os.execute "flatpak run nz.mega.MEGAsync & flatpak run md.obsidian.Obsidian & disown" end,
-	["all"] = function() dots() trave() dash() espaco() end
+	["all"] = function()
+		dots()
+		trave()
+		dash()
+		espaco()
+	end
 }
 
 if require "sai":ca() then arg[1] = "all" end
