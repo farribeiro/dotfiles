b3cb1baff088c8cf42cc60e5c577c089f52ad4c0
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2026-04-12 16:02:46 -0300

    Autoformater linha a linha

--- m3u.lua
+++ m3u.lua
@@ -6,5 +6,7 @@
 require "sai":ca_none()
 local filename = arg[1] .. ".m3u"
 local file = require "util".open_file(filename, "w")
-file:write("#EXTM3U\n") file:write(arg[2] .. "\n") file:close()
+file:write("#EXTM3U\n")
+file:write(arg[2] .. "\n")
+file:close()
 print(("%s criado com sucesso."):format(filename))
