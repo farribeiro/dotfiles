1588becf69c7ac37b43b6a214cafaecfc769411e
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2026-04-12 16:15:42 -0300

    Autoformater

--- nvim.lua
+++ nvim.lua
@@ -3,4 +3,5 @@
 -- SPDX-License-Identifier: GPL-2.0
 -- Copyright 2022-2023 - Fábio Rodrigues Ribeiro and contributors
 
-os.execute(("flatpak run io.neovim.nvim %s"):format(require "sai":ca() and " " or arg[1] == "s" and "${HOME}/.var/app/io.neovim.nvim/config" or ("%s/%s"):format(os.getenv("PWD"), arg[1])))
+os.execute(("flatpak run io.neovim.nvim %s"):format(require "sai":ca() and " " or
+	arg[1] == "s" and "${HOME}/.var/app/io.neovim.nvim/config" or ("%s/%s"):format(os.getenv("PWD"), arg[1])))
