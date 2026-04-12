b5a97597fc530beb35fd08efd97b62f9acabf586
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2026-04-12 16:15:09 -0300

    Autoformater

--- meme.lua
+++ meme.lua
@@ -4,4 +4,5 @@
 -- Copyright 2023 - Fábio Rodrigues Ribeiro and contributors
 
 require "sai":ca_none()
-os.execute(("ffmpeg -i %s -vf \"split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse\" -loop 0 %s"):format(arg[1], arg[1]:gsub("%.mp4", ".webp")))
+os.execute(("ffmpeg -i %s -vf \"split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse\" -loop 0 %s"):format(arg[1],
+	arg[1]:gsub("%.mp4", ".webp")))
