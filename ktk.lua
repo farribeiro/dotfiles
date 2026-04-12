fab1a83ba6fca48faffbccf98bd9835c2d140274
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2026-04-12 16:11:21 -0300

    Autoformater e refatorado

--- ktk.lua
+++ ktk.lua
@@ -3,4 +3,8 @@
 -- SPDX-License-Identifier: GPL-2.0
 -- Copyright 2025 - Fábio Rodrigues Ribeiro and contributors
 
-os.execute "sudo koji-tool install kernel"
+-- os.execute "koji-tool install kernel"
+local cmd = "koji-tool install kernel"
+local x = os.execute
+
+if require "sai".ca() then x(cmd) else x(cmd .. "*" .. require "util".get_nextkernelminorwithoutpatch() .. "*") end
