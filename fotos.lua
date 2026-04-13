ea5fcad844c735d9a8fc9fb00e6e8e4c4e99186a
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2026-04-12 22:17:12 -0300

    Autoformater

--- fotos.lua
+++ fotos.lua
@@ -10,18 +10,22 @@ local function ls() return util.getoutput([[\ls -1]]):gsub("[\n\r]", "") end
 local function mkdir() x("mkdir -p resultado") end
 
 local function corre(file, opt)
-	if file:match "%.avif$" or file:match "%.jpg$" or file:match "%.jpeg$" or file:match "%.png$" or file:match "%.webp$" or file:match "%.jxl$" then x(("convert %s"):format(opt)) end
+	if file:match "%.avif$" or file:match "%.jpg$" or file:match "%.jpeg$" or file:match "%.png$" or file:match "%.webp$" or file:match "%.jxl$" then
+		x(("convert %s"):format(opt))
+	end
 end
 
 local function resize(size)
-	mkdir() for file in ls() do corre(file, ([[-resize %s "%s" "resultado/%s"]]):format(size, file, file)) end
+	mkdir()
+	for file in ls() do corre(file, ([[-resize %s "%s" "resultado/%s"]]):format(size, file, file)) end
 end
 
 local function normalize_ppi()
-	mkdir() for file in ls() do corre(file, ([[-depth 72 "%s" "www/%s"]]):format(file, file)) end
+	mkdir()
+	for file in ls() do corre(file, ([[-depth 72 "%s" "www/%s"]]):format(file, file)) end
 end
 
-handlers ={
+handlers = {
 	["resize-800600"] = function() resize "800x600" end,
 	["resize-hd"] = function() resize "1280x720" end,
 	["normalize-ppi"] = function() normalize_ppi() end
