3ecaa4b68123a87ffea177b06b1f4d590f3af2c7
Fábio Rodrigues Ribeiro <farribeiro@gmail.com>
2026-05-04 23:26:51 -0300

    Fix path in getoutput_all function
    Updated the path in the getoutput_all function to remove an extra '/home/fribeiro'.

--- btrfs-balance.lua
+++ btrfs-balance.lua
@@ -5,7 +5,7 @@
 
 local x = os.execute
 
-local w = require "util".getoutput_all("echo -e '/var\n/var/home/fribeiro/Games' | gum filter --placeholder 'O que deseja fazer?'", "*a")
+local w = require "util".getoutput_all("echo -e '/var\n/var/home/fribeiro/Games' | gum filter --placeholder 'O que deseja fazer?'")
 x("sudo  btrfs filesystem usage " .. w)
 io.write("\n" .. ("*"):rep(10) .. "\nDusage: ")
 local n = io.read "*n" or 0
