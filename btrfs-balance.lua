f1638ed443808e8484849a50f112578e42e3caac
Fábio Rodrigues Ribeiro <farribeiro@gmail.com>
2026-05-08 17:27:13 -0300

    Fix variable usage and improve error handling

--- btrfs-balance.lua
+++ btrfs-balance.lua
@@ -4,10 +4,11 @@
 -- Copyright 2026 - Fábio Rodrigues Ribeiro and contributors
 
 local x = os.execute
+local u = require "util"
 x "sudo -s <<< 'journalctl --rotate && journalctl --vacuum-time=2d'"
-local w = require "util".getoutput_all("echo -e '/var\n/var/home/fribeiro/Games' | gum filter --placeholder 'O que deseja fazer?'")
+local w = u.getoutput_all("echo -e '/var\n/var/home/fribeiro/Games' | gum filter --placeholder 'O que deseja fazer?'")
 x("sudo  btrfs filesystem usage " .. w)
 io.write("\n" .. ("*"):rep(10) .. "\nDusage: ")
 local n = io.read "*n" or 0
-if n == 0 then print "Argumentos inválidos passados" os.exit(1) end
+u.ca_zero(n)
 x(("sudo btrfs balance start -dusage=%d %s"):format(n, w))
