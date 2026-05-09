270b97b3a3c8dc7053a912791001ee5d8ecbab4b
Fábio Rodrigues Ribeiro <farribeiro@gmail.com>
2026-05-08 21:32:14 -0300

    Refactor functions and return statement in sai.lua

--- sai.lua
+++ sai.lua
@@ -6,14 +6,6 @@ local function ca_exit()
 	io.write("Nenhum argumento foi passado, saindo...\n")
 	os.exit(1)
 end
-local function ca_none()
-	if ca() then ca_exit() end
-end
-local function ca_zero(n)
-	if n == 0 then ca_exit() end
-end
-return {
-	ca_none = ca_none,
-	ca_zero = ca_zero,
-	ca = ca
-}
+local function ca_none() if ca() then ca_exit() end end
+local function ca_zero(n) if n == 0 then ca_exit() end end
+return { ca_none = ca_none, ca_zero = ca_zero, ca = ca }
