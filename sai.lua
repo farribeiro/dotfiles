d283d034ad1bf00bde09bab21d6c4b6edc916cb6
Fábio Rodrigues Ribeiro <farribeiro@gmail.com>
2026-05-23 20:00:13 -0300

    Refactor argument handling functions in sai.lua

--- sai.lua
+++ sai.lua
@@ -1,11 +1,10 @@
 -- SPDX-License-Identifier: GPL-2.0
 -- Copyright 2024 - Fábio Rodrigues Ribeiro and contributors
-
-local function ca() return not arg or #arg == 0 end
 local function ca_exit()
-	io.write("Nenhum argumento foi passado, saindo...\n")
-	os.exit(1)
+    io.write("Nenhum argumento foi passado, saindo...\n")
+    os.exit(1)
 end
+local function ca() return not arg or #arg == 0 end
 local function ca_none() if ca() then ca_exit() end end
-local function ca_zero(n) if n == 0 then ca_exit() end end
+local function ca_zero(_) if _ == 0 then ca_exit() end end
 return { ca_none = ca_none, ca_zero = ca_zero, ca = ca }
