b4ac74df054345105c1f963cb2ed98ee9143454e
Fábio Rodrigues Ribeiro <farribeiro@gmail.com>
2026-05-24 07:04:17 -0300

    Fix command formatting in whatdepends handler

--- gum-fdep.lua
+++ gum-fdep.lua
@@ -1,8 +1,6 @@
 #!/usr/bin/env lua
-
 -- SPDX-License-Identifier: GPL-2.0
 -- Copyright 2025 - Fábio Rodrigues Ribeiro and contributors
-
 local op = io.open
 local x = os.execute
 local u = require "util"
@@ -13,7 +11,7 @@ local cmd_rq_pv = cmd_rq .. "%s --providers-of=%s"
 local handlers = {
 	["filepackage"] = function() return cmd_rq_pv:format(app_query, "provides") end,
 	["showdeps"] = function() return cmd_rq_pv:format(app_query, "depends") end,
-	["whatdepends"] = function() return cmd_rq .. ("--whatdepends %s"):format(app_query) end
+	["whatdepends"] = function() return ("%s --whatdepends %s"):format(cmd_rq, app_query) end
 }
 local choice = u.getoutput_all(
 	"echo -e 'filepackage\nshowdeps\nwhatsdepends\nCancelar' | gum filter --placeholder 'O que deseja fazer?'")
