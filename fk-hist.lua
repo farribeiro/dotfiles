c343adc1021671e9192768699b1ddc99f56343c8
Fábio Rodrigues Ribeiro <farribeiro@gmail.com>
2026-05-25 13:15:01 -0300

    Fix regex pattern in flatpak history command

--- fk-hist.lua
+++ fk-hist.lua
@@ -4,6 +4,6 @@
 local x = os.execute
 x 'gum spin --spinner dot --title "🔍 Buscando..." -- sleep 0.5'
 local tmp = os.tmpname()
-"flatpak history --reverse | sed -E 's/master|origin|uninstall//d;s/(deploy|stable|system.*$)//Ig' > " .. tmp
+"flatpak history --reverse | sed -E '/master|origin|uninstall//d;s/(deploy|stable|system.*$)//Ig' > " .. tmp
 x("gum filter --placeholder 'Entradas...' < " .. tmp)
 os.remove(tmp)
