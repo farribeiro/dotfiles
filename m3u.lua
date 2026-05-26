24952d13396a33ff379cfd07b35b2059a7db8c2f
Fábio Rodrigues Ribeiro <farribeiro@gmail.com>
2026-05-26 09:21:35 -0300

    Update m3u.lua

--- m3u.lua
+++ m3u.lua
@@ -4,8 +4,6 @@
 -- Copyright 2024 - Fábio Rodrigues Ribeiro and contributors
 
 require "sai":ca_none()
-local filename = arg[1] .. ".m3u"
-local f = io.open(filename, "w")
-f:write("#EXTM3U\n" .. arg[2] .. "\n")
-f:close()
+local f = assert(io.open(arg[1] .. ".m3u", "w"))
+f:write("#EXTM3U\n" .. arg[2] .. "\n"):close()
 print(("%s criado com sucesso."):format(filename))
