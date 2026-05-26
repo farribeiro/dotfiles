e660b60fb3ed321376cbd5c766e485d5b573f717
Fábio Rodrigues Ribeiro <farribeiro@gmail.com>
2026-05-26 07:22:16 -0300

    Update fk-hist.lua

--- fk-hist.lua
+++ fk-hist.lua
@@ -1,9 +1,7 @@
 #!/usr/bin/env lua
 -- SPDX-License-Identifier: GPL-2.0
 -- Copyright 2022-2026 - Fábio Rodrigues Ribeiro and contributors
-local x = os.execute
-x 'gum spin --spinner dot --title "🔍 Buscando..." -- sleep 0.5'
-local tmp = os.tmpname()
-x ("flatpak history --reverse | sed -E '/master|origin|uninstall/d;s/(deploy|stable|system.*$)//Ig' > " .. tmp)
-x("gum filter --placeholder 'Entradas...' < " .. tmp)
-os.remove(tmp)
+os.execute [[gum spin --spinner dot --title "🔍 Buscando..." -- sleep 0.5 &&\
+gum filter --placeholder '' \
+< <(flatpak history --reverse) \
+< <(sed -E '/master|origin|uninstall/d;s/(deploy|stable|system.*$)//Ig') < <(column -t)]]
