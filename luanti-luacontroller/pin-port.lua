591d4fd81ad93a084c2da5dd712cfb371ebaa67e
Fábio Rodrigues Ribeiro <farribeiro@gmail.com>
2026-06-23 16:53:42 -0300

    Refactor interrupt function for pin control

--- luanti-luacontroller/pin-port.lua
+++ luanti-luacontroller/pin-port.lua
@@ -1,6 +1,7 @@
 --Interrupt-Driven Clock
 --Continually pulses pin A, turning on/off once per second.
-local s = 5
-function i(clock) interrupt(s, clock, true) end
+function i(clock) 
 port.c = pin.a and true
-i("clock")
+interrupt(5, -- Segundos
+    clock,
+    true)
