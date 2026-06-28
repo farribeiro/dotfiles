8f70df1ca16b143f1871c7a391bca672fdcfa48b
Fábio Rodrigues Ribeiro <farribeiro@gmail.com>
2026-06-28 14:36:17 -0300

    Refactor interrupt handling in pin-port2.lua
    Refactor interrupt function and adjust pin logic.

--- luanti-luacontroller/pin-port2.lua
+++ luanti-luacontroller/pin-port2.lua
@@ -4,12 +4,9 @@
 --Interrupt-Driven Clock
 --Continually pulses pin A, turning on/off once per second.
 
-function i(_) interrupt(30, -- Segundos
-  _,
-  true) 
-end
-
 if pin.a then
   port.c = false
-  i("nc")
+  interrupt(30, -- Segundos
+    "nc",
+    true) 
 elseif event.iid == "nc" then port.c = true end
