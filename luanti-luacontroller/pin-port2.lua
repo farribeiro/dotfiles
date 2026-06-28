0b8dc8a73761e4eb5151ad16cfc284df0d631c40
Fábio Rodrigues Ribeiro <farribeiro@gmail.com>
2026-06-28 07:58:02 -0300

    Add interrupt-driven clock functionality to pin-port2.lua
    Implement an interrupt-driven clock that toggles pin A every second.

---
+++ luanti-luacontroller/pin-port2.lua
@@ -0,0 +1,18 @@
+-- SPDX-License-Indentifier: GPL-2.0
+-- Copyright 2024-2026 Fabio Rodrigues Ribeiro and contributors
+ 
+--Interrupt-Driven Clock
+--Continually pulses pin A, turning on/off once per second.
+
+function i(_) interrupt(30, -- Segundos
+  _,
+  true) 
+end
+
+if pin.a then
+  port.c = true
+  i("nc")
+elseif event.iid == "nc" then
+  port.c = not port.c
+  i("c")
+end
