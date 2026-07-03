ec6a8527757fe35d5b9c417d9c0a65e8c686180e
Fábio Rodrigues Ribeiro <farribeiro@gmail.com>
2026-07-03 17:03:11 -0300

    Refactor interrupt calls to use helper function

--- luanti-luacontroller/pin-port3.lua
+++ luanti-luacontroller/pin-port3.lua
@@ -4,23 +4,24 @@
 --Interrupt-Driven Clock
 --Continually pulses pin A, turning on/off once per second.
 
+local function i (s, _)
+  interrupt(s, _, true)
+end 
+
 if pin.c then
   port.b = false
   port.d = true
-  interrupt(1, -- Segundos
-  "1",
-  true)
+  i(1, -- Segundos
+  "1")
 elseif event.iid == "1" then
   port.d = false
   port.a = true
-  interrupt(1, -- Segundos
-  "2",
-  true) 
+  i(1, -- Segundos
+  "2") 
 elseif event.iid == "2" then
   port.a = false
   i(8, -- Segundos
-  "3",
-  true) 
+  "3") 
 elseif event.iid == "3" then
   port.b = true
 end
