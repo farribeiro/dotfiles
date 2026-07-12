77e28758af2ae0e636c61c5309ba6b43aaf596dc
Fábio Rodrigues Ribeiro <farribeiro@gmail.com>
2026-07-12 16:46:29 -0300

    Use util.escape for application query escaping

--- gum-rpmqa.lua
+++ gum-rpmqa.lua
@@ -3,12 +3,11 @@
 local op = io.open
 local x = os.execute
 local u = require "util"
-local function escape(str) return "'" .. str:gsub("'", "'\\''") .. "'" end
 -- Inicio
 local app_query = u.getoutput_all('gum input --placeholder "Digite o nome da aplicação"')
 if not app_query or app_query == "" then os.exit(0) end
 x('gum spin --spinner dot --title "🔍 Buscando..." -- sleep 0.5')
-local raw_list = u.getoutput_all("rpm -qa | grep " .. escape(app_query))
+local raw_list = u.getoutput_all("rpm -qa | grep " .. u.escape(app_query))
 if not raw_list or raw_list == "" then
 	print("⚠️  Nenhum app encontrado.")
 	os.exit(1)
