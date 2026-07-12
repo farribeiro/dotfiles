73e9c649598a0fca09e27681777677f1e05bb77e
Fábio Rodrigues Ribeiro <farribeiro@gmail.com>
2026-07-12 16:57:12 -0300

    Update btrfs balance script to use flatpak command

--- gum-btrfsbalance.lua
+++ gum-btrfsbalance.lua
@@ -3,9 +3,8 @@
 -- Copyright 2026 - Fábio Rodrigues Ribeiro and contributors
 local x = os.execute
 local u = require "util"
-local fk = "flatpak "
 local sbtrfs = "sudo btrfs "
-u.writecmd_x(fk .. "remove --unused --delete-data -y")
+u.writecmd_x("flatpak remove --unused --delete-data -y")
 u.writecmd_x "sudo -s <<< 'journalctl --rotate && journalctl --vacuum-time=2d'"
 local w = u.getoutput_all("echo -e '/var\n/var/home/fribeiro/Games' | gum filter --placeholder 'O que deseja fazer?'")
 local cmd_use = sbtrfs .. "filesystem usage " .. w
