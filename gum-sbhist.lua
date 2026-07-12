63a1468ced74e918dbe6b50b2406e0f7b45fdb93
Fábio Rodrigues Ribeiro <farribeiro@gmail.com>
2026-07-12 15:45:24 -0300

    Add sb hist

---
+++ gum-sbhist.lua
@@ -0,0 +1,11 @@
+#!/usr/bin/env lua
+
+-- SPDX-License-Identifier: GPL-2.0
+-- Copyright 2022-2026 - Fábio Rodrigues Ribeiro and contributors
+
+-- x('gum spin --spinner dot --title "🔍 Buscando..." -- sleep 0.5')
+local p_in = assert(io.popen("rpm -qa --nodigest --nosignature --last", "r"))
+local t = {}
+for l in p_in:lines() do t[#t + 1] = l end
+p_in:close()
+os.execute("gum pager << EOF\n" .. table.concat(display_list, "\n") .. "EOF")
