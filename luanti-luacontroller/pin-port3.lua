c0d1e78c15e442a990b58e11e4618f683dbd4727
Fábio Rodrigues Ribeiro <farribeiro@gmail.com>
2026-07-16 20:29:10 -0300

    Remove clock functionality comments from pin-port3.lua
    Removed comments about interrupt-driven clock functionality.

--- luanti-luacontroller/pin-port3.lua
+++ luanti-luacontroller/pin-port3.lua
@@ -1,9 +1,6 @@
 -- SPDX-License-Indentifier: GPL-2.0
 -- Copyright 2024-2026 Fabio Rodrigues Ribeiro and contributors
  
---Interrupt-Driven Clock
---Continually pulses pin A, turning on/off once per second.
-
 local i = interrupt
 if pin.c then
   port.b, port.d = true, false
