16601a8e2fc743c1d395745daee889562284499a
Fábio Rodrigues Ribeiro <farribeiro@gmail.com>
2026-07-23 10:09:20 -0300

    Refactor balance command into a function

--- gum-btrfsbalance.lua
+++ gum-btrfsbalance.lua
@@ -8,8 +8,10 @@ u.writecmd_x("flatpak remove --unused --delete-data -y")
 u.writecmd_x "sudo -s <<< 'journalctl --rotate && journalctl --vacuum-time=2d'"
 local w = u.getoutput_all("echo -e '/var\n/var/home/fribeiro/Games' | gum filter --placeholder 'O que deseja fazer?'")
 local cmd_use = sbtrfs .. "filesystem usage " .. w
-x(cmd_use)
-io.write("\n" .. ("*"):rep(10) .. "\nDusage: ")
-local n = io.read "*n"
-x(("%s balance start -dusage=%d %s"):format(sbtrfs, n, w))
-x(cmd_use)
+local function bal(_)
+	x(cmd_use)
+	io.write("\n" .. ("*"):rep(10) .."\n" .. _ .. "usage: ")
+	x(("%s balance start -%susage=%d %s"):format(sbtrfs, io.read "*n", w))
+end
+bal("d")
+bal("m")
