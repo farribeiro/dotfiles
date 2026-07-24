473c4661d3c1b08adc94b2b0668b4596f0775d55
Fábio Rodrigues Ribeiro <farribeiro@gmail.com>
2026-07-24 19:35:21 -0300

    Remove gnome-software quit message in upgrade
    Removed gnome-software quit message from upgrade function.

--- sb.lua
+++ sb.lua
@@ -26,9 +26,6 @@ end
 local function rpmostree_upgrade(opts)
 	kv()
 	lastdeploy()
-	if os.getenv("DESKTOP_SESSION") == "gnome" then
-		u.x_writemsg("gnome-software --quit", "\n*** Parado gnome-software\n\n")
-	end
 	x(("%s %s upgrade %s"):format(roc, ro, opts))
 end
 local function txup()
