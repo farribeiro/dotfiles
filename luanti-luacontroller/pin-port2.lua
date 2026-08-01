404d7abe007d839daaae9ffcaaa625dd35363f14
Fábio Rodrigues Ribeiro <farribeiro@gmail.com>
2026-07-31 23:00:34 -0300

    Fix syntax errors in pin-port2.lua

--- luanti-luacontroller/pin-port2.lua
+++ luanti-luacontroller/pin-port2.lua
@@ -1,11 +1,13 @@
 -- SPDX-License-Indentifier: GPL-2.0
 -- Copyright 2024-2026 Fabio Rodrigues Ribeiro and contributors
- 
+
 --Interrupt-Driven Clock
 --Continually pulses pin A, turning on/off once per second.
 
 if pin.a then
   port.c = false
   interrupt(30, -- Segundos
-    "nc") 
-elseif event.iid == "nc" then port.c = true end
+    "nc")
+elseif event.iid == "nc" then
+  port.c = true
+end
