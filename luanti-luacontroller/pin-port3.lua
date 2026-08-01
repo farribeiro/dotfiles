95b1bf9ae1be58fd4db512a2d45e15bf64cbee44
Fábio Rodrigues Ribeiro <farribeiro@gmail.com>
2026-07-31 22:59:08 -0300

    Fix syntax errors in pin-port3.lua

--- luanti-luacontroller/pin-port3.lua
+++ luanti-luacontroller/pin-port3.lua
@@ -1,18 +1,18 @@
 -- SPDX-License-Indentifier: GPL-2.0
 -- Copyright 2024-2026 Fabio Rodrigues Ribeiro and contributors
- 
+
 local i = interrupt
 if pin.c then
   port.b, port.d = true, false
   i(1, -- Segundos
-  "1")
+    "1")
 elseif event.iid == "1" then
   port.a = true
   i(1, -- Segundos
-  "2") 
+    "2")
 elseif event.iid == "2" then
   i(8, -- Segundos
-  "3") 
+    "3")
 elseif event.iid == "3" then
   port.a, port.b = false, false
   port.d = true
